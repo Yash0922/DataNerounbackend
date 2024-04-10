03a77f7edcb3167f4a848d00a6962a4b4d2d4216
yash0922 <yashkumarpal987@gmail.com>
2024-04-10 11:21:08 +0530

    deploy in firebase

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const cors = require("cors");
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const errorMiddleware = require('./Middlewares/error.middleware');
 const authRoutes = require('./Routes/authRoutes');
 const dataRoutes = require('./Routes/dataRoutes');
-
+const functions = require('firebase-functions');
 const app = express();
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://yashkumarpal987:Yash%4015182204@cluster0.ss532hp.mongodb.net/DataNeuron ', {
 });
-
+app.use(cors());
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
@@ -21,8 +22,9 @@ app.use('/api/data', dataRoutes);
 
 // Error handling middleware
 app.use(errorMiddleware);
-
-const PORT = process.env.PORT || 3000;
+exports.api = functions.https.onRequest(app)
+const PORT = process.env.PORT || 8423;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
